bef0027d3662964d7b8f36ec4d097e82b7d8a03c
Nik <solexx1@yandex.ru>
2020-04-22 23:09:22 +0300

    1_First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//
+'use strict';
+
+let a = 5;
+console.log(a);
+
+console.log(typeof(String(4)));
+
+console.log(typeof("ww" + 5));
+console.log('http://vk.com/catalog/' + 5);
+// to NUmber
+
+//console.log("arr" + "- object");
+
+let incr = 10,
+    decr = 10;
+
+// Префиксная форма
+//  console.log(++incr);
+ //   console.log(--decr);
+
+//постфикс
+console.log(incr++);
+console.log(decr--);
+
+
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose);
+//prompt("FU");
+
